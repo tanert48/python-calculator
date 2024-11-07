17d2dc7c60f86c66968da7bf35256fac962da8ad
tanert48 <iake1154@gmail.com>
2024-11-07 08:16:05 +0000

    modified calculator and test_calculator

--- calculator.py
+++ calculator.py
@@ -3,23 +3,31 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
+        negative = False
+        if b < 0:
+            negative = True
+            b = -b  
+        for i in range(b):
             result = self.add(result, a)
+        if negative:
+            result = -result
         return result
 
     def divide(self, a, b):
+        if b == 0:
+            return("Eror")
         result = 0
-        while a > b:
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
         return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
--- test_calculator.py
+++ test_calculator.py
@@ -6,10 +6,44 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
 
+
+#add
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
-
     # Add the following test methods to the TestCalculator class:
+    def test_add_zero(self):
+            self.assertEqual(self.calc.add(1, 0), 1)
+    def test_add_negative(self):
+        self.assertEqual(self.calc.add(-1, -2), -3)
+#sub
+    def test_sub(self):
+        self.assertEqual(self.calc.subtract(2, 1), 1)
+
+    def test_sub_negative(self):
+        self.assertEqual(self.calc.subtract(-1, -2), 1)
+
+    def test_sub_zero(self):
+        self.assertEqual(self.calc.subtract(-1, -2), 1)  
+#multi
+    def test_mul(self):
+        self.assertEqual(self.calc.multiply(2, 1), 2)
+
+    def test_mul_negative(self):
+        self.assertEqual(self.calc.multiply(-2, -2), 4)
+
+    def test_mul_zero(self):
+        self.assertEqual(self.calc.multiply(-5, 0), 0)
+#div
+    def test_div_zero(self):
+        self.assertEqual(self.calc.divide(5, 0), "Eror")
+    def test_div(self):
+        self.assertEqual(self.calc.divide(0, 5), 0)
+#mod
+    def test_mod(self):
+        self.assertEqual(self.calc.modulo(6, 5), 1)
+    def test_mod_same(self):
+        self.assertEqual(self.calc.modulo(6, 6), 0)
+    
 
 if __name__ == '__main__':
     unittest.main()
